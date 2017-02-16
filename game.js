b1e220b5a436bfea5ba0c23e23ee22422ee804c4
rob bee <rbenny@earthlink.net>
2017-02-16 10:32:35 -0500

    update js

---
+++ game.js
@@ -0,0 +1,29 @@
+var inquirer = require('inquirer')
+var Word = require('./Word.js');
+
+var words = ['jeff', 'john', 'rhyna','xxxyyyuuu'];
+
+var wordToPlay = words[Math.floor(Math.random()*words.length)];
+
+var wordObject = new Word(wordToPlay);
+wordObject.makeAndPushLettersIntoWord();
+console.log(wordObject.display());
+
+function askLetter(){
+    inquirer.prompt([
+    {
+    type: "input",
+    name: "guess",
+    message: "What letter do you guess? If you are done then say no."},
+    ]).then(function(data){
+        if (data.guess != 'no') {
+            wordObject.updateLetter(data.guess);
+
+            console.log(wordObject.display());
+
+            askLetter();
+        }
+    });
+}
+
+askLetter();
